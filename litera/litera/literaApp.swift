489ac63a654775df2adf034d9b1c3331a77b6ec2
Angelina Chen <angelina.chhen@gmail.com>
2024-12-02 17:50:22 -0500

    Retrying creation

---
+++ litera/litera/literaApp.swift
@@ -0,0 +1,17 @@
+//
+//  literaApp.swift
+//  litera
+//
+//  Created by Angelina Chen on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct literaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
