40be26ff9f39ccb0e1ebf7a61821c8482de9a8e7
Labhya Mehta <labhyamehta@gmail.com>
2024-12-06 17:39:51 -0500

    Test 2

--- litera/litera/Views/HomeView.swift
+++ litera/litera/Views/HomeView.swift
@@ -10,9 +10,8 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         NavigationStack {
-            Text("Get Started")
-            Text("Home Screen")
-                .navigationTitle("Home")
+          
+               
         }
     }
 }
